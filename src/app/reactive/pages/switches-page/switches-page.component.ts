edbc2189c4b2da99af299f5580a638318b1439b3
jean peralta <jean.peralta@igmweb.com>
2025-01-29 18:16:07 +0100

    Pagina switches tarea

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'reactive-switches-page',
@@ -8,5 +9,24 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class SwitchesPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true,Validators.required],
+    termAndConditions: [false, Validators.requiredTrue]
+  })
+
+  onSave(): void {
+    if( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
+
+  isValidField( field: string ) {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+  }
 
 }
